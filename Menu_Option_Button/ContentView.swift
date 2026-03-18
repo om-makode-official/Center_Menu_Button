f51774ff16bab7eb0f43b4c010ea35cdde2736e3
Om Kashinath Makode <omkashinath_m@nexteducation.in>
2026-03-18 13:27:39 +0530

    code updated

--- Menu_Option_Button/ContentView.swift
+++ Menu_Option_Button/ContentView.swift
@@ -8,14 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isExpanded = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 0){
+            
+            ZStack{
+                if isExpanded{
+                    Color.gray
+                        .ignoresSafeArea()
+                        .onTapGesture {
+                            toggleMenu()
+                        }
+                    
+                }
+                ZStack{
+                    menuOptions(icon: "folder", bgColor: .red)
+                        .offset(x: isExpanded ? 55 : 0)
+                        .opacity(isExpanded ? 1 : 0)
+                        .zIndex(isExpanded ? 1 : 0)
+                    menuOptions(icon: "paperplane.fill", bgColor: .blue)
+                        .offset(x: isExpanded ? 50 : 0, y: isExpanded ? -55 : 0)
+                        .opacity(isExpanded ? 1 : 0)
+                        .zIndex(isExpanded ? 1 : 0)
+                    menuOptions(icon: "doc.text.fill", bgColor: .orange)
+                        .offset(y: isExpanded ? -80 : 0)
+                        .opacity(isExpanded ? 1 : 0)
+                        .zIndex(isExpanded ? 1 : 0)
+                    menuOptions(icon: "bell.and.waveform", bgColor: .green)
+                        .offset(x: isExpanded ? -50 : 0, y: isExpanded ? -55 : 0)
+                        .opacity(isExpanded ? 1 : 0)
+                        .zIndex(isExpanded ? 1 : 0)
+                    menuOptions(icon: "icloud.and.arrow.down", bgColor: .yellow)
+                        .offset(x: isExpanded ? -55 : 0)
+                        .opacity(isExpanded ? 1 : 0)
+                        .zIndex(isExpanded ? 1 : 0)
+                    
+                    
+                    Button(action: {
+                        toggleMenu()
+                        
+                    }, label: {
+                        ZStack{
+                            Circle()
+                                .fill(Color.cyan)
+                                .frame(width: 48, height: 48)
+                            
+                            Image(systemName: "plus")
+                                .foregroundColor(Color.white)
+                                .frame(width: 30)
+                                .rotationEffect(.degrees(isExpanded ? 45 : 0))
+                        }
+                        .offset(y: isExpanded ? -20 : 0)
+                    })
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                .padding(.bottom, 20)
+            }
+            Rectangle()
+                .frame(height: 150)
         }
-        .padding()
+        .ignoresSafeArea()
+    }
+    
+    private func menuOptions(icon: String, bgColor: Color) -> some View{
+        Button(action: {
+            print("\(icon) tapped")
+        }, label: {
+            ZStack{
+                Circle()
+                    .fill(bgColor)
+                    .frame(width: 48, height: 48)
+                
+                Image(systemName: icon)
+                    .frame(width: 18, height: 24)
+                    .foregroundColor(Color.white)
+            }
+        })
+    }
+    
+    func toggleMenu(){
+        withAnimation(.spring(response: 0.4, dampingFraction: 0.7, blendDuration: 0)){
+            isExpanded.toggle()
+        }
     }
 }
 
