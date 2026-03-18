c442b87a5a0ee10431a1d99010de27d12e863a65
Om Kashinath Makode <omkashinath_m@nexteducation.in>
2026-03-18 12:14:06 +0530

    Initial Commit

---
+++ Menu_Option_Button/Menu_Option_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_Option_ButtonApp.swift
+//  Menu_Option_Button
+//
+//  Created by Sai Krishna on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Menu_Option_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
